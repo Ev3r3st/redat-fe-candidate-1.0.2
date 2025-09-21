c8b053904ad45c242a768c6999dc1865b97594ba
Ev3r3st <matykrotil@gmail.com>
2025-09-21 17:05:44 +0200

    chore(build): make BUILD_DIR env-driven with sensible default

--- webpack/constants.js
+++ webpack/constants.js
@@ -5,7 +5,7 @@ const ABSOLUTE_BASE = path.normalize(path.join(__dirname, '..'));
 const PATHS = {
   ABSOLUTE_BASE,
   NODE_MODULES_DIR: path.join(ABSOLUTE_BASE, 'node_modules'),
-  BUILD_DIR: path.join(ABSOLUTE_BASE, 'build/opt/redat/www/fe-candidate'),
+  BUILD_DIR: process.env.BUILD_DIR ? path.resolve(process.env.BUILD_DIR) : path.join(ABSOLUTE_BASE, 'build'),
   SRC_DIR: path.join(ABSOLUTE_BASE, 'src'),
   IMG_DIR: path.join(ABSOLUTE_BASE, 'img'),
   PROJECT_DIR: 'fe-candidate',
